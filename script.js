9d1b295fcdb2df03bb3a116c6f6ca315e27b4cad
bhuveen <naveenbhuvi6@gmail.com>
2024-05-13 11:57:01 -0700

    code changed

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const questionButtonElement = document.getElementById("answer-buttons");
 
+startButton.addEventListener("click",startGame);
+
+function startGame(){
+   startButton.classList.add("hide");
+}
+
+function setNextQuestion(){
+
+}
